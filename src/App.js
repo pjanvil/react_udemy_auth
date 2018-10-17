89a7ef37d31f91464efce972f39fa01d57132bac
Paul Jones <puj@Pauls-MacBook-Air.local>
2018-10-17 18:14:10 +0100

    Udemy lession 59 import firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+
 import { Header } from './components/common';
+
 class App extends Component {
+    componentWillMount() {
+        // Initialize Firebase
+        const config = {
+            apiKey: 'AIzaSyCCKDs4diOWb61h0pxCFJDmS2jS_R2Pcm0',
+            authDomain: 'authentication-251d0.firebaseapp.com',
+            databaseURL: 'https://authentication-251d0.firebaseio.com',
+            projectId: 'authentication-251d0',
+            storageBucket: 'authentication-251d0.appspot.com',
+            messagingSenderId: '367366164220'
+        };
+        firebase.initializeApp(config);
+    }
+
     render() {
         return (
             <View>
