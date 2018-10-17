0c47930a685239c7d08681d1838d921e6dd45aa6
Paul Jones <puj@Pauls-MacBook-Air.local>
2018-10-17 17:40:57 +0100

    56

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text>For oru clients</Text>
+            </View>
+        )
+    }
+}
+
+export default App;
