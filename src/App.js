d1131aec4c6c65d0ed9607987c346a4ffcbeaf81
Paul Jones <puj@Pauls-MacBook-Air.local>
2018-10-18 17:33:42 +0100

    lesson 65 added input text

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import firebase from 'firebase';
+//import firebase from 'firebase';
 
 import { Header } from './components/common';
+
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
     componentWillMount() {
         // Initialize Firebase
-        const config = {
+        /*
+        firebase.initializeApp({
             apiKey: 'AIzaSyCCKDs4diOWb61h0pxCFJDmS2jS_R2Pcm0',
             authDomain: 'authentication-251d0.firebaseapp.com',
             databaseURL: 'https://authentication-251d0.firebaseio.com',
             projectId: 'authentication-251d0',
             storageBucket: 'authentication-251d0.appspot.com',
             messagingSenderId: '367366164220'
-        };
-        firebase.initializeApp(config);
+        });
+        */
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>For oru clients</Text>
+                <LoginForm />
             </View>
         )
     }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import { Button, Card, CardSection, Input } from './common';
+
+class LoginForm extends Component {
+    state = { text: '' };
+
+    render() {
+        return (
+            <Card>
+                <CardSection>
+                    <Input
+                        label={'For Our Clients'}
+                        value={this.state.text}
+                        onChangeText={text => this.setState({ text })}
+                    />
+                </CardSection>
+                <CardSection />
+                <CardSection>
+                    <Button>
+                        Log In
+                    </Button>
+                </CardSection>
+            </Card>
+        );
+    }
+}
+
+export default LoginForm;
