9c5cd9384539c15f7a9672d07f01a516dae9f308
Paul Jones <puj@Pauls-MacBook-Air.local>
2018-10-22 14:05:48 +0100

    working with firebase enabled and error

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react';
 
 import { Text } from 'react-native';
 
-//import firebase from 'firebase';
+import firebase from 'firebase';
 
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state = { email: '', password: '', error };
+    state = { email: '', password: '', error: '' };
 
     onButtonPress() {
         const { email, password } = this.state;
-        /*
+
         firebase.auth().signInWithEmailAndPassword(email, password)
             .catch(() => {
                 firebase.auth().createUserWithEmailAndPassword(email, password)
                     .catch(() => {
                         this.setState({error: 'Authentication Failed.'});
                     });
-            });*/
+            });
     }
 
     render() {
